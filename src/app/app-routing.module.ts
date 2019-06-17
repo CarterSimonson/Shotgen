267bae72126d997f924e5ef4cd69d5020c3425aa
Carter <cartersimonson01@gmail.com>
2019-06-17 18:31:34 -0500

    Initial Commit 6/17/2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Import components
+import { EditorComponent } from './pages/editor/editor.component';
+
+const routes: Routes = [
+  { path: '', component: EditorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { MaterialModule } from './material.module';
+import { EditorComponent } from './pages/editor/editor.component';
+import { ImagePickerComponent } from './components/image-picker/image-picker.component';
+import { ColorPickerComponent } from './components/color-picker/color-picker.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    ImagePickerComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
